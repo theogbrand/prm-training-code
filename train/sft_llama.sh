4e60454e42015dfdf3019a7b9c64fc7a46f9640f
ob1-brandon <ob1-brandon@lambda-sj.org>
2025-07-15 08:51:35 +0000

    sft llama

---
+++ train/sft_llama.sh
@@ -0,0 +1,28 @@
+# Reference Running: bash train/sft.sh
+# {'train_runtime': 5268.8407, 'train_samples_per_second': 0.949, 'train_steps_per_second': 0.119, 'train_loss': 0.1172730620391667, 'epoch': 5.0}
+uid="$(date +%Y%m%d_%H%M%S)"
+base_model="meta-llama/Llama-3.2-11B-Vision-Instruct"
+dataset_name="ob11/ai2d-prm-training-data-v0.4-pil"
+lr=2e-5
+epochs=2
+micro_batch_size=8 # -> batch_size will be 64 if 8 gpus, per device batch size in single node
+gradient_accumulation_steps=8 # requires more GPU memory
+max_steps=-1
+min_lr=0 # -> not used now
+weight_decay=1e-4 # -> not used now
+gpu_count=$(nvidia-smi -L | wc -l)
+push_to_hub=false
+
+accelerate launch --config_file=train/deepspeed_zero3.yaml \
+    train/sft_llama_llava.py \
+    --dataset_name ${dataset_name} \
+    --model_name_or_path ${base_model} \
+    --per_device_train_batch_size ${micro_batch_size} \
+    --gradient_accumulation_steps ${gradient_accumulation_steps} \
+    --output_dir training_outputs/sft-llama-3.2-11b-vision-instruct-${uid} \
+    --bf16 True \
+    --torch_dtype bfloat16 \
+    --gradient_checkpointing \
+    --num_train_epochs ${epochs} 
+    # --gradient_checkpointing=True \ Enable gradient checkpointing for efficient memory usage with 8 H100 GPUs.
+    # --accelerator_config='{"gradient_accumulation_kwargs": {"sync_each_batch": true}}'
