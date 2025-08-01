cbd959fa49d8a7511dfbe8112cb9818064d4a12f
ob1-hopper <ob1-hopper@aisg.org>
2025-08-01 16:03:24 +0800

    created add tokens qwen

--- train/utils/add_tokens_to_model.py
+++ train/utils/add_tokens_to_model.py
@@ -1,34 +1,34 @@
 import os
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 
+
+model_id = 'Qwen/Qwen2.5-VL-7B-Instruct'
 tokenizer = AutoTokenizer.from_pretrained(
-    'Qwen/Qwen2.5-7B-Instruct'
+    model_id,
     )
 
-model = AutoModelForCausalLM.from_pretrained(
-    'Qwen/Qwen2.5-7B-Instruct', 
+model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+    model_id, 
     torch_dtype=torch.bfloat16, 
     device_map="cpu"
     )
 
-# Adding special tokens
 existing_special_tokens = tokenizer.special_tokens_map.get("additional_special_tokens", [])
 GOOD_TOKEN='<+>'
 BAD_TOKEN='<->'
-SEPERATOR_TOKEN='<extra>'
-new_special_tokens = [GOOD_TOKEN, BAD_TOKEN, SEPERATOR_TOKEN]
+# SEPERATOR_TOKEN='<extra>' # can use for MLM later
+new_special_tokens = [GOOD_TOKEN, BAD_TOKEN]
 updated_special_tokens = list(set(existing_special_tokens + new_special_tokens))
 tokenizer.add_special_tokens({"additional_special_tokens": updated_special_tokens})
 
 # Editing embedding of SEPERATOR_TOKEN
-with torch.no_grad():
-    extra_token_id = tokenizer.convert_tokens_to_ids(SEPERATOR_TOKEN)
-    base_token_id = tokenizer.pad_token_id  # or use "<mask>", "unused", etc.
-    model.get_input_embeddings().weight[extra_token_id] = model.get_input_embeddings().weight[base_token_id]
+# with torch.no_grad():
+#     extra_token_id = tokenizer.convert_tokens_to_ids(SEPERATOR_TOKEN)
+#     base_token_id = tokenizer.pad_token_id  # or use "<mask>", "unused", etc.
+#     model.get_input_embeddings().weight[extra_token_id] = model.get_input_embeddings().weight[base_token_id]
 
-
-save_directory = "./models/Qwen2.5-Math-7B-Instruct-updated"
+save_directory = "./models/Qwen2.5-VL-7B-Instruct-updated-tokens"
 
 os.makedirs(save_directory, exist_ok=True)
 
